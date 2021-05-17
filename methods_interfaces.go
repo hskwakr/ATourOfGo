aac9eb89e8c33f44d5f365d11a821735d7686206
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-05-17 17:09:25 +0900

    Update methods_interfaces.go

--- methods_interfaces.go
+++ methods_interfaces.go
@@ -13,18 +13,43 @@ func (v vertex2) abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func absFunc(v vertex2) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func tour35() {
 	v := vertex2{3, 4}
 	fmt.Println(v.abs())
 }
 
-func scale(v vertex2, f float64) {
+func (v *vertex2) scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func scaleFunc(v *vertex2, f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
 
 func tour36() {
 	v := vertex2{3, 4}
-	scale(v, 10)
+	v.scale(2)
+	scaleFunc(&v, 10)
+
+	p := &vertex2{4, 3}
+	p.scale(3)
+	scaleFunc(p, 8)
+
+	fmt.Println(v, p)
+}
+
+func tour37() {
+	v := vertex2{3, 4}
 	fmt.Println(v.abs())
+	fmt.Println(absFunc(v))
+
+	p := &vertex2{4, 3}
+	fmt.Println(p.abs())
+	fmt.Println(absFunc(*p))
 }
