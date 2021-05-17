4fc440d9a509bbf3d7dfe3234d4b7b9b5e3f07e0
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-05-17 18:03:18 +0900

    Update methods_interfaces.go

--- methods_interfaces.go
+++ methods_interfaces.go
@@ -91,7 +91,29 @@ func (t T) M() {
 	fmt.Println(t.S)
 }
 
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
 func tour39() {
 	var i I = T{"Hello"}
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func tour40() {
+	var i I
+
+	i = &T{"Hello"}
+	describe(i)
+	i.M()
+
+	i = F(math.Pi)
+	describe(i)
 	i.M()
 }
