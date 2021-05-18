2d6d3892c3c51a93c263cca11b65a73509f9ee6f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-05-18 18:28:53 +0900

    Studied type switch

--- methods_interfaces.go
+++ methods_interfaces.go
@@ -145,3 +145,20 @@ func tour42() {
 	f, ok := i.(float64)
 	fmt.Println(f, ok)
 }
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T\n", v)
+	}
+}
+
+func tour43() {
+	do(21)
+	do("hello")
+	do(true)
+}
