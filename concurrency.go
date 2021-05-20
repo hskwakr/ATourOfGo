78fc32267aa012b030c64a4eb7061014411d7ce9
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-05-20 22:31:02 +0900

    Studied channel

--- concurrency.go
+++ concurrency.go
@@ -16,3 +16,22 @@ func tour52() {
 	go say("world")
 	say("hello")
 }
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func tour53() {
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c
+
+	fmt.Println(x, y, x+y)
+}
